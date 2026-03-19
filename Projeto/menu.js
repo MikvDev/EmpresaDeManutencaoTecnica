9eb1eee866720e07937c45739bb03ac3affb1917
MIGUEL VARGAS FERREIRA <04631817003@senacrs.edu.br>
2026-03-19 14:41:09 -0300

    chore: Adiciona mysql2

---
+++ Projeto/menu.js
@@ -0,0 +1,14 @@
+export function menu(){
+    console.log("======  Bem Vindo ======")
+    console.log("                        ")
+    console.log("1. Cadastro de usuário  ")
+    console.log("                        ")
+    console.log("2. Cadastro de produto  ")
+    
+
+
+
+
+
+
+}
